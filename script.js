03df1327b39a0e72c97e449ddc3744827905c63b
f.evstigneev <f.evstigneev@realize.dev>
2023-01-05 18:27:25 +0600

    added vibrations (1)

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const button = document.querySelector(".pulsing_button");
+const tg = window.Telegram?.WebApp;
 
 let buttonActive = false;
+window.addEventListener("load", () => {
+    if (!tg) return;
+
+    if (!tg.isExpanded) {
+        tg.expand();
+    }
+})
 
 button.addEventListener("click", () => {
-    console.log("popip")
+    if (!tg) return;
+
+    const { impactOccurred } = tg?.HapticFeedback;
     buttonActive = !buttonActive;
 
     if (buttonActive) {
         button.classList.add("working");
+        button.innerHTML = "Working..."
     } else {
         button.classList.remove("working");
+        button.innerHTML = "PUSH ME"
     }
 })
+
+while (buttonActive) {
+    setTimeout(() => impactOccurred("soft"))
+}
